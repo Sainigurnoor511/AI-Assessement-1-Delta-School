3453d69f2bb979551ecf7b7f4018a1847ef05ef8
Sainigurnoor511 <sainigurnoor511@gmail.com>
2024-07-02 11:29:40 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -15,7 +15,7 @@ def divide(num1, num2):
     if num2 != 0:
         return num1 / num2
     else:
-        return "Error: Division by zero"
+        return "UNDIFINED: Division by zero is not possible."
 
 while True:
     print("Select operation:")
@@ -27,16 +27,23 @@ while True:
 
     choice = input("Enter choice: ")
 
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    
 
     if choice == '1':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
         result = add(num1, num2)
     elif choice == '2':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
         result = subtract(num1, num2)
     elif choice == '3':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
         result = multiply(num1, num2)
     elif choice == '4':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
         result = divide(num1, num2)
     elif choice == '5':
         exit()
